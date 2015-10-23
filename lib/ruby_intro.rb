7f557d32279757872a9e1785f9955e9b4fe14080
Marcelo Picolo Khoury <mpkhoury@gmail.com>
2015-10-23 18:59:58 +0000

    Part 1 completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,29 +4,46 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr.inject(:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+	return 0 if arr.empty?
+	return arr[0] if arr.length == 1
+	arr.sort!
+	return arr[ arr.length-1 ] + arr[ arr.length - 2 ]
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+
+def sum_to_n?(arr, n)
+	return false if arr.empty? || arr.length == 1
+	return arr.combination( 2 ).any? { | a, b | a + b == n }
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+ 
+ 
+    return true
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if s.empty?
+ 
+  return true if (s.length == 1 && s == "0")
+  
+  #Check if the last 2 digt's are 0's only
+  return s =~ ("00\z" && "\A[0|1]{3,}\z") 
+  
+
 end
 
 # Part 3
